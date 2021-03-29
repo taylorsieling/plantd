20c00c51bf50ddbf47eb4d26cc376d01ee04d347
taylorsieling <taylorasieling@gmail.com>
2021-03-29 11:43:35 -0500

    fetch plants js response

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
 const plantList = document.getElementById('plant-list')
 
 function fetchPlants() {
-
+    fetch('http://localhost:3000/plants')
+    .then(res => res.json())
+    .then(data)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
