ff682cf57e2012afe166520658cac19440fa9b88
taylorsieling <taylorasieling@gmail.com>
2021-03-29 10:52:50 -0500

    care controller methods index/show

--- backend/app/controllers/cares_controller.rb
+++ backend/app/controllers/cares_controller.rb
@@ -1,2 +1,13 @@
 class CaresController < ApplicationController
+
+    def index
+        all_care = Care.all
+        render json: all_care
+    end
+
+    def show
+        care = Care.find(params[:id])
+        render json: care
+    end
+    
 end
