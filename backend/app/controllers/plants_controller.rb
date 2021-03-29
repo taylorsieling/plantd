0087b19968d93f32a5e8f7adec23ea8c2467d273
taylorsieling <taylorasieling@gmail.com>
2021-03-29 10:27:21 -0500

    plant controller methods index/show

--- backend/app/controllers/plants_controller.rb
+++ backend/app/controllers/plants_controller.rb
@@ -1,9 +1,13 @@
 class PlantsController < ApplicationController
 
     def index
+        plants = Plant.all
+        render json: plants 
     end
 
     def show
+        plant = Plant.find(params[:id])
+        render json: plant
     end
-    
+
 end
