6f6789592b558628e196d1420701cbf843d2a56e
taylorsieling <taylorasieling@gmail.com>
2021-03-29 14:06:25 -0500

    created care constructor

---
+++ frontend/src/care.js
@@ -0,0 +1,17 @@
+class Care {
+
+    static all = []
+
+    constructor(care_type, notes, date, id, plant_id) {
+        this.care_type = care_type
+        this.notes = notes
+        this.date = date
+        this.id = id
+        this.plant_id = plant_id
+        
+        this.element = document.createElement('div')
+        this.element.id = `care-${this.id}`
+
+        Care.all.push(this)
+    }
+}
