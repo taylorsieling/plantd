2783d5dbb26520d88efad2078e4b972c732053a2
taylorsieling <taylorasieling@gmail.com>
2021-03-29 14:04:02 -0500

    created plant constructor

--- frontend/src/plant.js
+++ frontend/src/plant.js
@@ -1,7 +1,19 @@
 // Contain Plant Class
 
 class Plant {
-    constructor() {
-        
+
+    static all = []
+    
+    constructor(species, nickname, description, pot, id) {
+        this.species = species
+        this.nickname = nickname
+        this.description = description
+        this.pot = pot
+        this.id = id
+
+        this.element = document.createElement('div')
+        this.element.id = `plant-${this.id}`
+
+        Plant.all.push(this)
     }
 }
