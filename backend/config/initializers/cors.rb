2c2959e265339e0137668f40a384d12b7cc3cc22
taylorsieling <taylorasieling@gmail.com>
2021-03-29 12:03:30 -0500

    added plants to DOM

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost'
+    # fix this later
+    origins '*'
 
     resource '*',
       headers: :any,
--- frontend/index.js
+++ frontend/index.js
@@ -3,9 +3,25 @@ const plantList = document.getElementById('plant-list')
 function fetchPlants() {
     fetch('http://localhost:3000/plants')
     .then(res => res.json())
-    .then(data)
+    .then(data => addPlants(data))
+}
+
+function addPlants(response) {
+    console.log(response)
+    response.data.forEach(plant => addPlantToDOM(plant))
+}
+
+function addPlantToDOM(plant) {
+    console.log(plant)
+    plantList.innerHTML += `
+        <div id="plant-${plant.id}>
+            <li> 
+            </li>
+        </div>`
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchPlants()
 })
+
+// Nickname - bold, Species, Description, Pot
