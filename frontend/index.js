ba99e7c5efca730fe5fe9351221b615669badb8d
taylorsieling <taylorasieling@gmail.com>
2021-03-29 22:21:30 -0500

    moved create new plant functions to plantAdapter

--- frontend/index.js
+++ frontend/index.js
@@ -1,44 +1,10 @@
 const plantList = document.getElementById('plant-list')
 const plantForm = document.getElementById('plant-form')
-const plantSpecies = document.getElementById('plant-species')
-const plantNickname = document.getElementById('plant-nickname')
-const plantDesc = document.getElementById('plant-description')
-const plantPot = document.getElementById('plant-pot')
 const plantsAdapter = new PlantsAdapter
-
-
-function handleCreatePlantSubmit(e) {
-    e.preventDefault()
-
-    let newPlantObj = {
-        nickname: plantNickname.value,
-        species: plantSpecies.value,
-        description: plantDesc.value,
-        pot: plantPot.value
-    }
-
-    let configObj = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(newPlantObj)
-    }
-
-    fetch('http://localhost:3000/plants/', configObj)
-    .then(res => res.json())
-    .then(response => {
-        addPlantToDOM(response.data)
-    })
-
-    console.log("Created a new plant!")
-    plantForm.reset()
-}
 
 
 document.addEventListener('DOMContentLoaded', () => {
     plantsAdapter.fetchPlants()
-    plantForm.addEventListener('submit', handleCreatePlantSubmit)
+    plantForm.addEventListener('submit', plantsAdapter.handleCreatePlantSubmit)
     plantList.addEventListener('click', plantsAdapter.handleListClick)
 })
--- frontend/src/plantsAdapter.js
+++ frontend/src/plantsAdapter.js
@@ -17,6 +17,41 @@ class PlantsAdapter {
         })
     }
 
+    handleCreatePlantSubmit = (e) => {
+        e.preventDefault()
+
+        const plantSpecies = document.getElementById('plant-species')
+        const plantNickname = document.getElementById('plant-nickname')
+        const plantDesc = document.getElementById('plant-description')
+        const plantPot = document.getElementById('plant-pot')
+    
+        let newPlantObj = {
+            nickname: plantNickname.value,
+            species: plantSpecies.value,
+            description: plantDesc.value,
+            pot: plantPot.value
+        }
+    
+        let configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(newPlantObj)
+        }
+    
+        fetch(this.baseUrl, configObj)
+        .then(res => res.json())
+        .then(response => {
+            let plant = new Plant(response.data.attributes)
+            plant.addPlantsToDom()
+        })
+    
+        console.log("Created a new plant!")
+        plantForm.reset()
+    }
+
     deletePlant(id) {
 
         let configObj = {
