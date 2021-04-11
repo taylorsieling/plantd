4e824fc1c4e82dd97ee84b8c80633cb7986cf1df
taylorsieling <taylorasieling@gmail.com>
2021-04-11 18:04:51 -0500

    fixing updateCareOnDom

--- frontend/src/care.js
+++ frontend/src/care.js
@@ -27,11 +27,12 @@ class Care {
         this.addEventListeners();
     }
 
+
     careFullRender() {
         let showDiv = document.getElementById(`plant-care`)
-        showDiv.innerHTML= `
-            <div class="care-card" id="care-${this.id}">
-                <div id="care-info">
+        let careInfo = `
+            <div class="care-card" id="care-card-${this.id}">
+                <div id="care-${this.id}-info">
                 <h2>${this.care_type}</h2>
                 <p>
                     ${this.notes}<br><br>
@@ -39,9 +40,28 @@ class Care {
                 </p>
                 </div>
                 <div id="care-buttons">
-                    <button class="update-care button" name="update-care" data-id="${this.id}">Update</button>
-                    <button class="delete-care button" name="delete-care" data-id="${this.id}">Delete</button>
+                    <button class="update-care button" name="update-care-${this.id}" data-id="${this.id}">Update</button>
+                    <button class="delete-care button" name="delete-care-${this.id}" data-id="${this.id}">Delete</button>
                 <div>
+            </div>
+        `
+        let careDiv = document.createElement('div')
+        careDiv.className = `care-${this.id}`
+        careDiv.innerHTML = careInfo
+        showDiv.append(careDiv)
+    }
+
+    renderUpdatedCare() {
+        let careInfo = document.getElementById(`care-${this.id}-info`)
+        careInfo.innerHTML = `
+            <div id="care-${this.id}">
+                <div id="care-${this.id}-info">
+                    <h2>${this.care_type}</h2>
+                    <p>
+                        ${this.notes}<br><br>
+                        <strong>Date:</strong> ${this.date}
+                    </p>
+                </div>
             </div>
         `
     }
@@ -50,11 +70,11 @@ class Care {
         this.care_type = care_type
         this.date = date
         this.notes = notes
-        this.careFullRender()
+        this.renderUpdatedCare()
     }
 
     addUpdateCareForm(id) {
-        let care = document.getElementById(`care-${id}`)
+        let care = document.getElementById(`care-card-${id}`)
         let updateForm = `
             <input type="text" value="${this.care_type}" name="care-type" id="update-care-type-${id}">
             <input type="text" value="${this.date}" name="date" id="update-date-${id}">
