db704a0a170ad769791df188af08894218ceb1ad
taylorsieling <taylorasieling@gmail.com>
2021-04-13 12:43:56 -0500

    cleaning up code in care.js

--- frontend/src/care.js
+++ frontend/src/care.js
@@ -51,72 +51,14 @@ class Care {
     }
 
     addEventListeners() {
-        this.deleteButton.addEventListener('click', this.handleButtonClick)
+        this.deleteButton.addEventListener('click', this.deleteCareClick)
     }
 
-    handleButtonClick = (e) => {
+    deleteCareClick = (e) => {
         // delete care
-            console.log('clicked delete care')
-            let careId = e.target.dataset.id
-            caresAdapter.deleteCare(careId)
-        // update care
-        // } else if (e.target.className === "update-care button") {
-        //     console.log('clicked update care')
-        //     let careId = e.target.dataset.id
-        //     e.target.className = "save-care button"
-        //     e.target.innerHTML = "Save"
-        //     this.addUpdateCareForm(careId)
-         // save updated care
-        // } else if (e.target.className === "save-care button") {
-        //     let careId = e.target.dataset.id
-        //     e.target.className = "update-care button"
-        //     e.target.innerHTML = "Update"
-        //     caresAdapter.updateCare(careId)
-        
+        console.log('clicked delete care')
+        let careId = e.target.dataset.id
+        caresAdapter.deleteCare(careId)
     }
-
-    // renderUpdatedCare() {
-    //     let careInfo = document.getElementById(`care-${this.id}-info`)
-    //     careInfo.innerHTML = `
-    //             <h2>${this.care_type}</h2>
-    //             <p>
-    //                 ${this.notes}<br><br>
-    //                 <strong>Date:</strong> ${this.date}
-    //             </p>
-    //     `
-    //     let careFormDiv = document.getElementById(`care-update-form-${this.id}`)
-    //     if (careFormDiv.style.display === 'none') {
-    //         careFormDiv.style.display = 'block';
-    //     } else if (careFormDiv.style.display = 'block') {
-    //         careFormDiv.reset()
-    //         careFormDiv.style.display = 'none';
-    //     }
-    // }
-
-    // updateCareOnDom({care_type, date, notes}) {
-    //     this.care_type = care_type
-    //     this.date = date
-    //     this.notes = notes
-    //     this.renderUpdatedCare()
-    // }
-
-    // addUpdateCareForm(id) {
-    //     let care = document.getElementById(`care-card-${id}`)
-    //     let updateCareForm = `
-    //         <input type="text" value="${this.care_type}" name="care-type" id="update-care-type-${id}">
-    //         <input type="text" value="${this.date}" name="date" id="update-date-${id}">
-    //         <input type="text" value="${this.notes}" name="notes" id="update-notes-${id}">
-    //     `
-    //     let careFormDiv = document.createElement('div')
-    //     careFormDiv.id = `care-update-form-${id}`
-    //     careFormDiv.className = 'care-update-form'
-    //     careFormDiv.innerHTML = updateCareForm
-    //     care.append(careFormDiv)
-
-    //     let saveButton = document.getElementsByClassName('update-care button')
-    //     saveButton.className = 'save-button button'
-    //     saveButton.innerHTML = 'Save'
-    // }
-    
 
 }
